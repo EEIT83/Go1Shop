84d27bad77187a5f0b29a689b4a87b43edd5530a
HengHao <HengHao@HengHao-PC>
2016-03-10 13:22:05 +0800

    修改後台密碼

--- Go1Shop/src/gb01_login/model/AdminDAO.java
+++ Go1Shop/src/gb01_login/model/AdminDAO.java
@@ -18,7 +18,7 @@ public class AdminDAO implements AdminFace {
 	private String SELECT_ID = "select * from admin where ad_mail=?";
 	private String SELECT_ALL = "select * from admin WHERE competence > 1";
 	private String INSERT = "insert into admin (ad_mail, ad_pwd, competence) values (?, ?, ?)";
-	private StringBuilder UPDATE = new StringBuilder("update admin set ");
+	private String UPDATE = "update admin set ad_pwd=? where ad_mail=? ";
 	private String DELETE = "DELETE FROM admin WHERE ad_mail=? ";
 
 	SQLserver db;
@@ -33,7 +33,8 @@ public class AdminDAO implements AdminFace {
 	public void insert(AdminBean AdVo) throws Exception {
 		try {
 			if (AdVo != null) {
-				Object params[] = { AdVo.getAd_mail(), AdVo.getAd_pwd(), AdVo.getCompetence() };
+				Object params[] = { AdVo.getAd_mail(), AdVo.getAd_pwd(),
+						AdVo.getCompetence() };
 				runner.update(db.getConn(), INSERT, params);
 			}
 		} catch (SQLException e) {
@@ -50,7 +51,7 @@ public class AdminDAO implements AdminFace {
 			List<AdminBean> result = runner.query(db.getConn(), SELECT_ID, rsh,
 					params);
 			if (result.size() <= 0) {
-				System.out.println("result.size()="+result.size());
+				System.out.println("result.size()=" + result.size());
 				return null;
 			} else {
 				return result.get(0);
@@ -85,59 +86,53 @@ public class AdminDAO implements AdminFace {
 	}
 
 	@Override
-	public void update(AdminBean AdVo) throws Exception {
-		List<Object> adList = new ArrayList<Object>();
-		
-		if (StringUtils.isNoneEmpty(AdVo.getAd_pwd())) {
-			UPDATE.append("ad_pwd=?, ");
-			adList.add(AdVo.getAd_pwd());
-		}
+	public void update(String mail , String pwd) throws Exception {
+		Object[] params = { pwd, mail };
 
-		UPDATE.delete(UPDATE.length() - 2, UPDATE.length());
-		UPDATE.append(" where mail=? ");
-		adList.add(AdVo.getAd_mail());
-
-		int count = runner.update(db.getConn(), UPDATE.toString(),
-				adList.toArray());
+		int count = runner.update(db.getConn(), UPDATE, params);
 		if (count <= 0) {
 			throw new Exception("Update error");
 		}
 	}
-//	public List<AdminBean> getByProperty(AdminBean AdVo) throws Exception {
-//		ResultSetHandler<List<AdminBean>> rsh = new BeanListHandler<AdminBean>(
-//				AdminBean.class);
-//		List<Object> params = new ArrayList<Object>();
-//		StringBuilder sb = new StringBuilder("select * from admin where competence > 1");
-//		if (StringUtils.isNotEmpty(AdVo.getAd_mail())){
-//			sb.append(" AND ad_mail=?");
-//			params.add(AdVo.getAd_mail());
-//		}
-//		if (StringUtils.isNotEmpty(AdVo.getAd_name())) {
-//			sb.append(" AND ad_name Like ?");
-//			params.add("%" + AdVo.getAd_name() + "%");
-//		}
-//		try {
-//			return runner.query(db.getConn(), sb.toString(), rsh,
-//					params.toArray());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw new Exception("查尋失敗" + e.getMessage());
-//		}
-//	}
+
+	// public List<AdminBean> getByProperty(AdminBean AdVo) throws Exception {
+	// ResultSetHandler<List<AdminBean>> rsh = new BeanListHandler<AdminBean>(
+	// AdminBean.class);
+	// List<Object> params = new ArrayList<Object>();
+	// StringBuilder sb = new
+	// StringBuilder("select * from admin where competence > 1");
+	// if (StringUtils.isNotEmpty(AdVo.getAd_mail())){
+	// sb.append(" AND ad_mail=?");
+	// params.add(AdVo.getAd_mail());
+	// }
+	// if (StringUtils.isNotEmpty(AdVo.getAd_name())) {
+	// sb.append(" AND ad_name Like ?");
+	// params.add("%" + AdVo.getAd_name() + "%");
+	// }
+	// try {
+	// return runner.query(db.getConn(), sb.toString(), rsh,
+	// params.toArray());
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// throw new Exception("查尋失敗" + e.getMessage());
+	// }
+	// }
 
 	public static void main(String[] args) throws Exception {
 		AdminBean bean = new AdminBean();
 		AdminDAO dao = new AdminDAO();
-		bean.setAd_mail("test@gmail.com");
-		//bean.setAd_pwd("test@gmail.com");
-		//bean.setAd_name("test");
-		//bean.setCompetence(2);
-		//dao.delete("");
-		//dao.insert(bean);
-		//List<AdminBean> results = dao.getByProperty(bean);
-//		for (AdminBean result : results) {
-//			System.out.println(result);
-//		}
+		//bean.setAd_mail("test@gmail.com");
+		// bean.setAd_pwd("test@gmail.com");
+		// bean.setAd_name("test");
+		// bean.setCompetence(2);
+//		dao.update("abc@gmail.com", "a");
+//		System.out.println(bean);
+		// dao.delete("");
+		// dao.insert(bean);
+		// List<AdminBean> results = dao.getByProperty(bean);
+//		 for (AdminBean result : results) {
+//		 System.out.println(result);
+//		 }
 
 	}
 
